acc4ff2f7ed783b0edeb99eba1ad45b49a37eeb5
Scott White <scottswhite90@gmail.com>
2024-06-29 23:24:23 -0500

    Update Advanced R Chapter 18.R

--- Advanced R Chapter 18.R
+++ Advanced R Chapter 18.R
@@ -13,3 +13,32 @@ x <- 4
 eval(z)
 
 y
+
+
+
+# Section 18.3.2 Symbols
+
+expr(x)
+
+sym("x")
+
+
+
+x <- 1:10
+
+call2(median, x, na.rm = TRUE)
+call2(expr(median), x, na.rm = TRUE)
+call2(median, expr(x), na.rm = TRUE)
+call2(expr(median), expr(x), na.rm = TRUE)
+
+
+
+
+f1 <- function(x, y) {
+  exprs(x = x, y = y)
+}
+f2 <- function(x, y) {
+  enexprs(x = x, y = y)
+}
+f1(a + b, c + d)
+f2(a + b, c + d)
